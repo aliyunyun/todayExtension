75b4a6b52c6c1925431e2a67fd53049347cdbf98
yuan yunlong <yuanyunlong@yuandeiMac.local>
2016-09-22 14:11:27 +0800

    init

---
+++ ToDoListKit/ToDoListKit.h
@@ -0,0 +1,19 @@
+//
+//  ToDoListKit.h
+//  ToDoListKit
+//
+//  Created by yuan yunlong on 16/9/21.
+//  Copyright © 2016年 yuan yunlong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ToDoListKit.
+FOUNDATION_EXPORT double ToDoListKitVersionNumber;
+
+//! Project version string for ToDoListKit.
+FOUNDATION_EXPORT const unsigned char ToDoListKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ToDoListKit/PublicHeader.h>
+
+
---
+++ swift3Project/TaskService.swift
@@ -0,0 +1,14 @@
+//
+//  TakeService.swift
+//  swift3Project
+//
+//  Created by yuan yunlong on 16/9/21.
+//  Copyright © 2016年 yuan yunlong. All rights reserved.
+//
+
+import Foundation
+
+let TaskServiceDataKey = "TaskServiceDataKey"
+public struct TakeService {
+    
+}
